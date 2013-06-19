bb33860c296ab1bdbaff743819a78357763230e4
cygnusnow <ruohuyang@gmail.com>
2013-06-20 00:21:38 +0800

    Add new click event for register button and login button. but it still not work.

--- src/EasySeller/src/com/activetassels/easyseller/MainActivity.java
+++ src/EasySeller/src/com/activetassels/easyseller/MainActivity.java
@@ -1,24 +1,103 @@
 package com.activetassels.easyseller;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 
 public class MainActivity extends Activity {
+	
+	private static final String TAG = "Login Activity";
+	private Button buttonRegister = null;
+	private Button buttonLogin = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate");
+        
+        buttonRegister = (Button)findViewById(R.id.register_button);
+        buttonRegister.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(MainActivity.this, register.class);
+				Log.d(TAG, "onClick register button");
+				startActivity(intent);
+				buttonRegister.setMovementMethod(LinkMovementMethod.getInstance());
+			}
+		});
+        
+        buttonLogin = (Button)findViewById(R.id.login_button);
+        buttonLogin.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(MainActivity.this, Login.class);
+				Log.d(TAG, "onClick login button");
+				startActivity(intent);
+				buttonLogin.setMovementMethod(LinkMovementMethod.getInstance());
+			}
+		});
+        
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+        Log.d(TAG, "onCreateOptionsMenu");
         return true;
     }
     
+    @Override
+    protected void onStart() {
+    	// TODO Auto-generated method stub
+    	super.onStart();
+    	Log.d(TAG, "onStart");
+    }
+    
+    @Override
+    protected void onStop() {
+    	// TODO Auto-generated method stub
+    	super.onStop();
+    	Log.d(TAG, "onStop");
+    }
+    
+    @Override
+    protected void onRestart() {
+    	// TODO Auto-generated method stub
+    	super.onRestart();
+    	Log.d(TAG, "onRestart");
+    }
+    
+    @Override
+    protected void onResume() {
+    	// TODO Auto-generated method stub
+    	super.onResume();
+    	Log.d(TAG, "onResume");
+    }
+    
+    @Override
+    protected void onPause() {
+    	// TODO Auto-generated method stub
+    	super.onPause();
+    	Log.d(TAG, "onPause");
+    }
+    
+    @Override
+    protected void onDestroy() {
+    	// TODO Auto-generated method stub
+    	super.onDestroy();
+    	Log.d(TAG, "onDestroy");
+    }
 }
